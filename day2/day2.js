a232c6f57eadd011e6f42fb66de586bc331c2e2a
Joshua <superaisystems@gmail.com>
2021-12-02 16:53:54 +0000

    Day 2 - Part one complete

---
+++ day2/day2.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('./day2.txt', 'utf8').trim().split('\n')
+
+const board = {
+  horizontal: 0,
+  depth: 0
+}
+
+function move(direction_object) {
+  switch(direction_object.direction) {
+    case 'forward':
+      board.horizontal += direction_object.times;
+      break
+    case 'down':
+      board.depth += direction_object.times;
+      break
+    case 'up':
+      board.depth -= direction_object.times;
+  }
+}
+
+function split_command(command) {
+  const array1 = command.split(' ')
+  return {
+    direction: array1[0],
+    times: Number(array1[1])
+  }
+}
+
+
+for (const command of data) {
+  move(split_command(command))
+}
+console.log(board.horizontal, board.depth) // 1905, 907
+console.log(1905*907)
