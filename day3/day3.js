248309ec3682277cf63e5b7f4339efb4167c5cfa
Joshua Bellew <joshua.bellew@dazn.com>
2021-12-03 15:43:56 +0000

    Day 3 - Part 2 complete

--- day3/day3.js
+++ day3/day3.js
@@ -1,9 +1,18 @@
 const {assert}=require('console');
 const fs = require('fs');
-const {CLIENT_RENEG_LIMIT}=require('tls');
 
-const diagnosticReport = fs.readFileSync('./input.txt', 'utf8').trim().split('\n')
-
+let diagnosticReport = fs.readFileSync('./input.txt', 'utf8').trim().split('\n')
+// let diagnosticReport = ['00100','10110',
+//   '11110',
+//   '10111',
+//   '10101',
+//   '01111',
+//   '00111',
+//   '11100',
+//   '10000',
+//   '11001',
+//   '00010',
+//   '01010']
 class Position {
   constructor() {
     this.up = 0;
@@ -19,59 +28,14 @@ class Position {
   }
   
   mostCommonBit() {
-    return this.up > this.down ? '1' : '0';
+    return this.up >= this.down ? '1' : '0';
   }
   
   leastCommonBit() {
-    return this.up > this.down ? '0' : '1';
+    return this.up >= this.down ? '0' : '1';
   }
 }
 
-function countNumberOfBits() {
-  for(const binaryString of diagnosticReport) {
-    for(let i=0;i<binaryString.length;i++) {
-      const currentPosition=getCurrentPosition(i);
-
-      currentPosition.incrementPosition(binaryString[i]);
-    }
-  }
-}
-
-const totalPositions = [];
-
-const getCurrentPosition = (index) => {
-  if (totalPositions[index]) {
-    return totalPositions[index];
-  } else {
-    totalPositions.push(new Position());
-    return totalPositions[index]
-  }
-}
-
-countNumberOfBits();
-
-const tally = tallyNumberOfBits();
-
-function tallyNumberOfBits() {
-  let gammaRate = '';
-  let epsilonRate = '';
-  for(const pos of totalPositions) {
-    gammaRate+=pos.mostCommonBit();
-    epsilonRate+=pos.leastCommonBit();
-  }
-  
-  return { gammaRate, epsilonRate }
-}
-
-function getGammaRateForPosition(position) {
-  return position.mostCommonBit()
-}
-const gammaRateNum = parseInt(tally.gammaRate, 2);
-const epsilonRateNum = parseInt(tally.epsilonRate, 2);
-
-const powerConsumption = gammaRateNum * epsilonRateNum;
-assert(powerConsumption === 3242606);
-// console.log(powerConsumption);
 /*
 Gamma rate: 101111001110
 Gamma number 3022
@@ -79,3 +43,76 @@ Epsilon rate: 010000110001
 Epsilon number 1073
 Total power: 3242606
 */
+/*
+1. Loop over each row
+2. remove row if bits dont match
+3. Once rows for first bit have been removed, recalculate next bit position, most common bit
+*/
+
+function calcMostCommonBitForColumn(fullReport, columnPosition) {
+  const result = new Position();
+  for (const row of fullReport) {
+    result.incrementPosition(row[columnPosition]);
+  }
+  return result.mostCommonBit()
+}
+
+function calcLeastCommonBitForColumn(fullReport, columnPosition) {
+  const result = new Position();
+  for (const row of fullReport) {
+    result.incrementPosition(row[columnPosition]);
+  }
+  return result.leastCommonBit()
+}
+
+function oxygenGenerator(diagnosticReport) {
+  for(let bitPos=0; bitPos < diagnosticReport[0].length; bitPos++) {
+    let mostCommonBit=calcMostCommonBitForColumn(diagnosticReport,bitPos);
+    console.log(mostCommonBit);
+
+    diagnosticReport=diagnosticReport.filter((diagRow) => {
+      if(diagRow[bitPos]==mostCommonBit) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+  return diagnosticReport
+}
+
+
+
+function CO2Rating(diagnosticReport) {
+  const length = diagnosticReport.length;
+  for(let bitPos=0; bitPos < length; bitPos++) {
+    let leastCommonBit = calcLeastCommonBitForColumn(diagnosticReport,bitPos);
+    // console.log(leastCommonBit ) ;
+
+    diagnosticReport = diagnosticReport.filter((diagRow) => {
+      if(diagnosticReport.length === 1){return true}
+      if(diagRow[bitPos] == leastCommonBit) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
+  return diagnosticReport
+}
+
+
+
+const oxygenRating = oxygenGenerator(diagnosticReport);
+console.log(oxygenRating); // oxygen = '101110111101'
+
+// console.log(diagnosticReport);
+const co2Scrubber = CO2Rating(diagnosticReport);
+console.log(co2Scrubber)
+
+
+function calculateBinaryNumbers(binary1, binary2) {
+  return parseInt(binary1, 2) * parseInt(binary2, 2)
+}
+
+console.log(calculateBinaryNumbers(oxygenRating, co2Scrubber));
